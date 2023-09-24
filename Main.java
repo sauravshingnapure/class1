6a151b417bbb03d72f8b4451653754e2fc8cab45
sauravshingnapure <145906038+sauravshingnapure@users.noreply.github.com>
2023-09-24 17:18:04 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean flag = true;
+        char c = 'q';
+        int a=3;
+        short s = 4500;
+        long r = 832433242;
+        float f =422.36f;
+        double d= 555555.65;
+        byte t = 2;
+        String str ="amay" ;
+
+        System.out.println("this is a boolea variable : "+flag);
+        System.out.println("this is a character : "+c);
+        System.out.println("This is a integer : "+a);
+        System.out.println("This is a short :"+s);
+        System.out.println("This is a long : "+r);
+        System.out.println("This is a float : "+f);
+        System.out.println("This is a Double : "+d);
+        System.out.println("This is a byte : "+t);
+        System.out.println("This is a string : "+str);
+
+    }
+}
